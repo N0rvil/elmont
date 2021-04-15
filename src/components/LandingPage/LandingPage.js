a4658bc93e6f673e59f335aa4aaa061f5866adef
Filip Duda <dudafilip80@seznam.cz>
2021-04-15 12:55:43 +0200

    fixing responsive bugs with navbar and footer

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -8,14 +8,14 @@ import './LandingPage.scss'
 
 const LandingPage = () =>{
     return (
-        <div className='landingpage' style={{ backgroundImage: "url('/images/landingpage-bg.png')"}}>
+        <div className='landingpage'>
             <Header />
-            <Navbar />       
-                <div className='landingpage__box'>
+            <Navbar />    
+            <div className='landingpage__content' style={{ backgroundImage: "url('/images/landingpage-bg.png')"}}>   
                     <h1 className='landingpage__header'>ren-dek</h1>
                     <div className='landingpage__line'></div>
-                    <h3 className='landingpage__text'>kvalitní elektroinstalace</h3>
-                </div> 
+                    <h3 className='landingpage__text'>kvalitní elektroinstalace</h3>      
+            </div>
             <Footer />
         </div>
     ) 
